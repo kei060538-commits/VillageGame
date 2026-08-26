bd5983ff00142dac383b2703f960bd614eaabf11
kei060538-commits <kei060538@gmail.com>
2026-08-27 04:07:29 +0900

    Verify bundled Web font has Japanese glyphs

---
+++ tests/web_font_validation.gd
@@ -0,0 +1,30 @@
+extends SceneTree
+
+const FONT_PATH := "res://art/fonts/NotoSansJP-Regular.otf"
+const REQUIRED_CODEPOINTS := [0x65E5, 0x3042, 0x9B54] # 日, あ, 魔
+
+func _init() -> void:
+    call_deferred("_run")
+
+func _run() -> void:
+    if not ResourceLoader.exists(FONT_PATH):
+        _fail("bundled Japanese font resource is missing")
+        return
+
+    var resource := load(FONT_PATH)
+    if not (resource is Font):
+        _fail("bundled Japanese font did not import as Font")
+        return
+
+    var font: Font = resource as Font
+    for codepoint in REQUIRED_CODEPOINTS:
+        if not font.has_char(int(codepoint)):
+            _fail("bundled Japanese font lacks U+%04X" % int(codepoint))
+            return
+
+    print("WEB FONT VALIDATION PASS: bundled font contains Japanese glyphs")
+    quit(0)
+
+func _fail(message: String) -> void:
+    push_error("WEB FONT VALIDATION FAIL: " + message)
+    quit(1)
